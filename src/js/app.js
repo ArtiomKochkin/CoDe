0d836a5568abf40cdd03747f17426270a1897f05
ArtiomKochkin <artiom.kochkin@yandex.ru>
2024-03-23 19:30:42 +0700

    feat: add fancybox for gallery

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,11 @@ import showMap from "./components/map.js";
 import handleModal from "./components/modal.js";
 import handleMenu from "./components/menu.js";
 import initSlider from "./components/introSlider.js";
+import initGallery from "./components/gallery.js";
 
 functions.isWebp();
 initSlider();
+initGallery();
 handleMenu();
 handleModal();
 showMap();
---
+++ src/js/components/gallery.js
@@ -0,0 +1,5 @@
+export default function initGallery() {
+    Fancybox.bind("[data-fancybox]", {
+        hideScrollbar: true,
+    });
+}
